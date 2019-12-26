43953324342c8780a93920c5e7bdaa7d961ad0a0
dedww <starykh.alexandr@gmail.com>
2019-12-26 21:29:35 +0300

    new commit

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -18,3 +18,5 @@ def test_sample_multiple_words():
 def test_generate_buzz_of_at_least_five_words():
     phrase = generator.generate_buzz()
     assert len(phrase.split()) >= 5
+
+    # comment to update git
